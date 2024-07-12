f5c307b9b0cf83ed280cb3a107da67c1758cff3d
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-07-12 21:58:38 +0530

    link fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import ShineBorder from './magicui/shine-border';
 export default function About() {
     return (
         <div className='flex flex-col justify-start items-center min-h-screen gap-10'>
-            <p className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-white/100 bg-clip-text text-center text-5xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 mt-17'>ABOUT US</p>
+            <p className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-white/100 bg-clip-text text-center text-5xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 mt-17'>ABOUT KARNA</p>
             <BlurFade delay={0.1} inView className="flex justify-center w-full">
                 <ShineBorder
                     className="relative flex h-[500px] w-[220px] sm:w-[350px] md:w-[500px] lg:w-[700px] flex-col justify-center items-center overflow-hidden rounded-lg border bg-background md:shadow-x shadow-[0_10px_50px_rgba(8,_112,_184,_0.7)]"
